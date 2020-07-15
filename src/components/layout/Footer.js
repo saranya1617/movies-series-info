2aa95eb2d74e831e07a4ec1ce006522612f989b1
saranya <vellankisaranya1617@gmail.com>
2020-07-15 19:31:32 +0530

    movieinfo.com

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -8,10 +8,9 @@ function Footer() {
           <div className="footer p-3 mt-4 text-center bg-dark text-light">
             Developed By:
             <span className="text-warning font-weight-normal">
-              Yaser AlAzem
+              saranya
             </span>
-            , Using <i className="fab fa-react" /> React JS &amp; Redux JS
-            integrated with external movies data API
+            , Using <i className="fab fa-react" /> React JS &amp; 
             <a
               href="http://www.omdbapi.com/"
               target="_blank"
